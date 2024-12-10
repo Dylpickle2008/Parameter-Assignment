12fbf10bc042f5f81e9531814c8b1d022949b484
Dylpickle2008 <dylanrigby1016@gmail.com>
2024-12-10 13:05:10 +0000

    init commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function product(paramA, paramB){
+    return paramA * paramB;
+}
+
+function sum(paramC, paramD){
+    return (paramC + paramD) * (1/2);
+}
+
+console.log(product(500,6));
+console.log(sum(6,4));
